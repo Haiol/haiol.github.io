9644afce14910457dc1311b972d209076e8723b4
Haiol <lovehp30@gmail.com>
2021-12-02 12:40:27 +0900

    21_12_02 add experience

---
+++ src/components/About.js
@@ -0,0 +1,142 @@
+import React, { Component } from "react";
+import { Icon } from "@iconify/react";
+import angularIcon from "@iconify/icons-logos/angular-icon";
+import reactIcon from "@iconify/icons-logos/react";
+import vueIcon from "@iconify/icons-logos/vue";
+import Lottie from 'lottie-web';
+import animate from 'lottie-web';
+
+
+class About extends Component {
+  constructor(props){
+    super(props);
+    this.state = { width: 0,height: 0};
+    this.container = React.createRef();
+  }
+  componentDidMount(){
+    const height = this.divElement.clientHeight;
+    const width = this.divElement.clientWidth;
+    console.log(height, width)
+    this.setState({height, width});
+    Lottie.loadAnimation({
+      container: this.container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./thinking.json'),
+    });
+    animate.setSpeed(0.5);
+
+  }
+
+  render() {
+
+
+    if (this.props.sharedBasicInfo) {
+      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+    }
+    if (this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.about;
+      var hello = this.props.resumeBasicInfo.description_header;
+      var about = this.props.resumeBasicInfo.description;
+      var about1 = this.props.resumeBasicInfo.description_sub1;
+      var about2 = this.props.resumeBasicInfo.description_sub2;
+    }
+
+    return (
+      
+      <section id="about">
+       
+        <div className="col-md-12">
+          <h1 style={{ color: "black" }}>
+            <span>{sectionName}</span>
+          </h1>
+          <div className="row center mx-auto mb-5">
+            <div className="col-md-4 mb-5 center">
+              <div className="polaroid">
+                <span style={{ cursor: "auto" }}>
+                  <img
+                    height="250px"
+                    src={profilepic}
+                    alt="Avatar placeholder"
+                  />
+             
+                  <Icon
+                    icon={angularIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={reactIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                  <Icon
+                    icon={vueIcon}
+                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                  />
+                </span>
+              </div>
+            </div>
+
+            <div className="col-md-8 center">
+              <div className="col-md-10" >
+              <span className='container' ref={this.container} style={{
+                      position:"absolute",
+                       top:"-50%",
+                       left:"-10%",
+                       width:"500px",
+                       height:"500px"
+                       }}/>
+                <div className="card">
+                  <div className="card-header">
+                    <span
+                      className="iconify"
+                      data-icon="emojione:red-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:yellow-circle"
+                      data-inline="false"
+                    ></span>{" "}
+                    &nbsp;{" "}
+                    <span
+                      className="iconify"
+                      data-icon="twemoji:green-circle"
+                      data-inline="false"
+                    ></span>
+                  </div>
+                  <div
+                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    style={{
+                      height: "auto",
+                      fontSize: "132%",
+                      lineHeight: "200%"
+                    }}
+                    ref={(divElement) => { this.divElement = divElement}}
+                  >
+                 
+                    <br />
+                    <span className="wave">{hello} :) </span>
+                    <br />
+                    <br />
+                    {about}
+                    <br />
+                    {about1}
+                    <br />
+                    {about2}                   
+                  </div>
+                  
+                      
+                   
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
