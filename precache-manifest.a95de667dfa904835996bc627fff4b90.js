6b82030a657da532bb176a64d134a517bfc0b864
Haiol <lovehp30@gmail.com>
2021-12-06 22:58:33 +0900

    Updates

---
+++ precache-manifest.a95de667dfa904835996bc627fff4b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d4e623306c69a0bc528583867bef7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43d5fdea76dbd33287d7",
+    "url": "/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "2190a77299940cf541b2",
+    "url": "/static/css/main.14e5a3ac.chunk.css"
+  },
+  {
+    "revision": "43d5fdea76dbd33287d7",
+    "url": "/static/js/2.94844120.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/static/js/2.94844120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2190a77299940cf541b2",
+    "url": "/static/js/main.444f1b87.chunk.js"
+  },
+  {
+    "revision": "6e5a731afbd78a096119",
+    "url": "/static/js/runtime-main.70d5ea23.js"
+  }
+]);
